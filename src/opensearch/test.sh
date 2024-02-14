3367a8bd87f9dfc6d0029174d9a34a3b9468c250
MyNameIsAndrew-Mangix <andrewklinsic@gmail.com>
2024-02-14 14:45:09 -0500

    fixed typo that caused container crash

--- src/opensearch/test.sh
+++ src/opensearch/test.sh
@@ -3,7 +3,7 @@ docker rm -f test-opensearch
 # starts a new docker container named test-opensearch with the following environment variables
 # --detach runs the container in the background
 #plugs.security.disabled=true allows us to test using http
-docker run -e discovery.type=single-node -e cluser.name=mentorHub -e plugins.security.disabled=true -p 9200:9200 --name test-opensearch --detach opensearchproject/opensearch:latest
+docker run -e discovery.type=single-node -e cluster.name=mentorHub -e plugins.security.disabled=true -p 9200:9200 --name test-opensearch --detach opensearchproject/opensearch:latest
 
 # export the following environment variables
 export PROTOCOL=http
