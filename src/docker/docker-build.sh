46698cb45efddb0c73b401d988d4f540e849ced5
Mike Storey <mike@ssgpp.com>
2024-02-16 20:20:21 -0500

    Moved Dockerfile and got entrypoint working

--- src/docker/docker-build.sh
+++ src/docker/docker-build.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 # Build Docker Image
-docker build --file src/docker/Dockerfile --tag ghcr.io/agile-learning-institute/mentorhub-<<TODO:domaion>>-<<TODO:component>>:latest .
+docker build --file src/docker/Dockerfile --tag ghcr.io/agile-learning-institute/mentorhub-search-opensearch:latest .
 if [ $? -ne 0 ]; then
     echo "Docker build failed"
     exit 1
 fi
 
 # Run the containers
-curl https://raw.githubusercontent.com/agile-learning-institute/mentorhub/main/docker-compose/<<run-local-TODO:.sh | /bin/bash
+# curl https://raw.githubusercontent.com/agile-learning-institute/mentorhub/main/docker-compose/<<run-local-TODO:.sh | /bin/bash
+docker run -e discovery.type=single-node -e cluster.name=mentorHub -e plugins.security.disabled=true -p 9200:9200 --name test-opensearch --detach opensearchproject/opensearch:latest
