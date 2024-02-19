1149e056ca82ff65290c81581c7ab0c7eb2af09a
MyNameIsAndrew-Mangix <andrewklinsic@gmail.com>
2024-02-19 15:38:05 -0500

    checks if docker-build is used in the right directory

--- src/docker/docker-build.sh
+++ src/docker/docker-build.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-# Build Docker Image
+echo "Ensure we are running in the proper folder"
+if !([[ -d "./src/docker" ]] && [[ -d "./src/opensearch" ]]); then 
+    echo "This script must be run from the repository root folder"
+    exit 1
+fi
+
+echo "Building Docker Image"
 docker build --file src/docker/Dockerfile --tag ghcr.io/agile-learning-institute/mentorhub-search-opensearch:latest .
 if [ $? -ne 0 ]; then
     echo "Docker build failed"
@@ -9,5 +15,6 @@ fi
 
 # Run the containers
 # curl https://raw.githubusercontent.com/agile-learning-institute/mentorhub/main/docker-compose/<<run-local-TODO:.sh | /bin/bash
-docker run -e discovery.type=single-node -e cluster.name=mentorHub -e plugins.security.disabled=true -p 9200:9200 --name test-opensearch --detach opensearchproject/opensearch:latest
-docker run ghcr.io/agile-learning-institute/mentorhub-search-opensearch:latest
+docker run -e discovery.type=single-node -e cluster.name=mentorHub -e plugins.security.disabled=true -p 9200:9200 --name mentorhub-opensearch-1 --detach opensearchproject/opensearch:latest
+sleep 15 # Ensure the opensearch container is open
+docker run --name mentorhub-opensearch-indexer-1 ghcr.io/agile-learning-institute/mentorhub-search-opensearch:latest 
