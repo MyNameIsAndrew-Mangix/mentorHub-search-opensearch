97b8acdf4a2db484dafaf9d23173e8ecdb0d5dd7
MyNameIsAndrew-Mangix <andrewklinsic@gmail.com>
2024-02-20 21:31:50 -0500

    applied fixes

--- src/docker/docker-build.sh
+++ src/docker/docker-build.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 echo "Ensure we are running in the proper folder"
-if !([[ -d "./src/docker" ]] && [[ -d "./src/opensearch" ]]); then 
+if !([[ -d "./src/docker" ]] && [[ -d "./src/searchinit" ]]); then 
     echo "This script must be run from the repository root folder"
     exit 1
 fi
 
 echo "Building Docker Image"
-docker build --file src/docker/Dockerfile --tag ghcr.io/agile-learning-institute/mentorhub-search-opensearch:latest .
+docker build --file src/docker/Dockerfile --tag ghcr.io/agile-learning-institute/mentorhub-search-searchinit:latest .
 if [ $? -ne 0 ]; then
     echo "Docker build failed"
     exit 1
