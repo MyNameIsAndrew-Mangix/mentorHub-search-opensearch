2e926cc23e080c5c25d95571ae5254152cdc9e7d
MyNameIsAndrew-Mangix <andrewklinsic@gmail.com>
2024-02-22 21:46:53 -0500

    updated test.sh for opensearch 2.12.0

--- src/searchinit/test.sh
+++ src/searchinit/test.sh
@@ -7,7 +7,7 @@ mh down
 # starts a new docker container named test-opensearch with the following environment variables
 # --detach runs the container in the background
 #plugs.security.disabled=true allows us to test using http
-docker run -e discovery.type=single-node -e cluster.name=mentorHub -e plugins.security.disabled=true -p 9200:9200 --name test-opensearch --detach opensearchproject/opensearch:latest
+docker run -e discovery.type=single-node -e cluster.name=mentorHub -e plugins.security.disabled=true -e OPENSEARCH_INITIAL_ADMIN_PASSWORD="55CKoK;9|'g{8i<4|Gny6pUX" -p 9200:9200 --name test-opensearch --detach opensearchproject/opensearch:2.12.0
 
 
 
